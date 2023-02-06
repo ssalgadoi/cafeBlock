625f8b9f86dcdb916dda2f4c8ce230ba22791ae9
IbacacheSalgado <se.salgado@duocuc.cl>
2023-02-06 14:45:16 -0300

    modificamos html con querySelector

---
+++ js/scripst.js
@@ -0,0 +1,6 @@
+// seleccionar contenido de un html
+//este nos devueve uno o ninguno de lo que estamos seleccionando
+//esta funcion nos sirve para manipular todo el html con JavaScript
+const heading = document.querySelector('.header__texto h2')//nos retorna 0 0 1
+
+console.log(heading);
